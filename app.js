a9aff078c924162c82738483873f1b7015a348b7
david kibe <kibek02@gmail.com>
2019-09-28 14:12:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const correctAnswer = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    userAnswers.forEach((answer, index)=>{
+        if(answer === correctAnswer[index]){
+            score += 25;
+        }
+    });
+    console.log(score);
+
+    scrollTo(0, 0)
+   
+
+    let output = 0;
+
+    const timer = setInterval(()=>{
+        result.querySelector('span').textContent = `${output}%`
+        if(output === score){
+            clearInterval(timer);
+        }else{
+            output++;
+        }
+
+    }, 25)
+});
+
+echo "# new" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/davidkibek/new.git
+git push -u origin master
+
+
+
